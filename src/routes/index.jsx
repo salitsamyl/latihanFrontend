39bd855e81408ebe161cc2d027d7a3589057746d
salitsamyl <salitsameylani@gmail.com>
2026-04-25 11:46:41 +0700

    praktikum reactNative

---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import Dashboard from '../pages/Dashboard';
+import ProdukList from '../pages/ProdukList';
+
+function MainRoutes() {
+    return (
+    <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/produk" element={<ProdukList />} />
+    </Routes>
+    );
+}
+
+export default MainRoutes;
